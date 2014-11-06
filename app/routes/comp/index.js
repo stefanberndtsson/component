ac01c06825f47952a872f06c0dc85be13dcd0ac0
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-11-06 23:06:27 +0100

    Fix searching

--- app/routes/comp/index.js
+++ app/routes/comp/index.js
@@ -12,5 +12,19 @@ export default Ember.Route.extend({
 	return this.store.find('result', params).then(function(data) {
 	    return data;
 	});
+    },
+    setupController: function(controller, model) {
+	controller.set('model', model);
+	controller.set('searchQuery', controller.get('query'));
+
+	if(controller.get('page') > controller.get('model.meta.pagination.pages')) {
+	    controller.transitionToRoute('comp.index', {queryParams: {page: 1}});
+	    controller.set('page', 1);
+	}
+    },
+    actions: {
+	search: function(query) {
+	    this.transitionTo('comp.index', {queryParams: {query: query}});
+	}
     }
 });
