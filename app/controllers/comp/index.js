2755c35801283115eca384f1e5c8fa24d8da1994
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-11-06 21:41:55 +0100

    Make mobile work better

--- app/controllers/comp/index.js
+++ app/controllers/comp/index.js
@@ -1,6 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+    needs: ['application'],
     queryParams: ['page', 'query'],
-    itemController: 'comp.item'
+    itemController: 'comp.item',
+    isMobileBinding: 'controllers.application.isMobile',
+    isDesktopBinding: 'controllers.application.isDesktop',
+    sortProperties: ['name'],
+    pageArray: function() {
+	var pArray = [];
+	for(var i=0;i<this.get('model.meta.pagination.pages');i++) {
+	    var p = {page: i+1};
+	    if(this.get('model.meta.pagination.page') === i+1) {
+		p['active'] = true;
+	    }
+	    pArray.push(p);
+	}
+	return Ember.ArrayProxy.create({content: Ember.A(pArray)});
+    }.property('model.meta.pagination.pages', 'model.meta.pagination.page'),
+    pageStart: function() {
+	var currentPage = this.get('model.meta.pagination.page');
+	var perPage = this.get('model.meta.pagination.per_page');
+	if(!perPage) { return 0; }
+	return ((currentPage-1) * perPage)+1;
+    }.property('model.meta.pagination.page', 'model.meta.pagination.per_page'),
+    pageEnd: function() {
+	var currentPage = this.get('model.meta.pagination.page');
+	var perPage = this.get('model.meta.pagination.per_page');
+	var total = this.get('model.meta.query.total');
+	if(!perPage) { return 0; }
+	var pageEnd = ((currentPage-1) * perPage)+perPage;
+	if(pageEnd > total) {
+	    return total;
+	} else {
+	    return pageEnd;
+	}
+    }.property('model.meta.pagination.page', 'model.meta.pagination.per_page', 'model.meta.query.total'),
+    singleResult: function() {
+	if(this.get('model.meta.query.total') === 1) { return true; }
+	return false;
+    }.property('model.meta.query.total'),
 });
