5d74b5413dbfefc69d7fd4e576f3b355b6b362a0
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-11-06 22:29:59 +0100

    Clean up indentation a bit

--- config/environment.js
+++ config/environment.js
@@ -1,51 +1,51 @@
 /* jshint node: true */
 
 module.exports = function(environment) {
-  var ENV = {
-    modulePrefix: 'component-nodata',
-    environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      }
-    },
+    var ENV = {
+	modulePrefix: 'component-nodata',
+	environment: environment,
+	baseURL: '/',
+	locationType: 'auto',
+	EmberENV: {
+	    FEATURES: {
+		// Here you can enable experimental features on an ember canary build
+		// e.g. 'with-controller': true
+	    }
+	},
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+	APP: {
+	    // Here you can pass flags/options to your application instance
+	    // when it is created
+	}
+    };
+
+    if (environment === 'development') {
+	// ENV.APP.LOG_RESOLVER = true;
+	ENV.APP.LOG_ACTIVE_GENERATION = true;
+	// ENV.APP.LOG_TRANSITIONS = true;
+	// ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+	ENV.APP.LOG_VIEW_LOOKUPS = true;
+	ENV.contentSecurityPolicyHeader = 'Disabled-Content-Security-Policy';
+	ENV.APP.authenticationBaseURL = 'http://localhost:3019/session';
+	ENV.APP.serviceURL = 'http://localhost:3019';
+	ENV.APP.fileURL = 'http://localhost:3019/asset_data';
     }
-  };
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.contentSecurityPolicyHeader = 'Disabled-Content-Security-Policy';
-      ENV.APP.authenticationBaseURL = 'http://localhost:3019/session';
-      ENV.APP.serviceURL = 'http://localhost:3019';
-      ENV.APP.fileURL = 'http://localhost:3019/asset_data';
-  }
+    if (environment === 'test') {
+	// Testem prefers this...
+	ENV.baseURL = '/';
+	ENV.locationType = 'auto';
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'auto';
+	// keep test console output quieter
+	ENV.APP.LOG_ACTIVE_GENERATION = false;
+	ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+	ENV.APP.rootElement = '#ember-testing';
+    }
 
-    ENV.APP.rootElement = '#ember-testing';
-  }
+    if (environment === 'production') {
 
-  if (environment === 'production') {
+    }
 
-  }
-
-  return ENV;
+    return ENV;
 };
