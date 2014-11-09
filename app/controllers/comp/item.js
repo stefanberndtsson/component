41ffe70625401bd8d3939fac22c9601da1d257d9
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-11-09 15:24:56 +0100

    API simplified to remove some relational data and present readable entries instead. Description split into summary and description, with summary being a required field

--- app/controllers/comp/item.js
+++ app/controllers/comp/item.js
@@ -8,36 +8,24 @@ export default Ember.ObjectController.extend({
     isMobileBinding: 'controllers.application.isMobile',
     isDesktopBinding: 'controllers.application.isDesktop',
     needsValue: function() {
-	var amountObject = this.get('amounts')[this.get('model.amount_id')];
-	if(!amountObject) { return false; }
-	return amountObject.must_have_value;
-    }.property('model.amount_id'),
-    descriptionSummary: function() {
-	if(!this.get('model.description')) {
-	    return "";
+	if(this.get('model.amount') === "Fixed") {
+	    return true;
+	} else {
+	    return false;
 	}
-	var lines = this.get('model.description').split(/\n/);
-	return lines[0];
-    }.property('model.description'),
-    descriptionRest: function() {
-	if(!this.get('model.description')) {
-	    return "";
-	}
-	var lines = this.get('model.description').split(/\n/);
-	lines.shift();
-	return lines.join('  \n');
-    }.property('model.description'),
+    }.property('model.amount'),
     amount: function() {
-	if(!this.get('model.amount_id')) {
+	var amount = this.get('model.amount');
+	if(!amount) {
 	    return undefined;
 	}
-	var amountObject = this.get('amounts')[this.get('model.amount_id')];
-	if(amountObject.must_have_value) {
+
+	if(amount === "Fixed") {
 	    return this.get('model.amount_value');
 	} else {
-	    return amountObject.name;
+	    return amount;
 	}
-    }.property('model', 'model.amount_id', 'model.amount_value'),
+    }.property('model.amount', 'model.amount_value'),
     tagsArray: function() {
 	return this.get('model.tags').map(function(item) {
 	    return {name: item, query: 'tags:'+item};
--- app/helpers/markdown-text.js
+++ app/helpers/markdown-text.js
@@ -1,5 +1,6 @@
 import Ember from "ember";
 
 export default Ember.Handlebars.makeBoundHelper(function(value) {
+    value = value || "";
     return new Ember.Handlebars.SafeString(markdown.toHTML(value));
 });
