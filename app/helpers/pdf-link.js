54aca1548bbe0b8c94e10b3dcc82bca8bb093afe
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-11-12 21:27:16 +0100

    Add a prescaled image because firefox seems to be completely incapable of doing proper scaling

--- app/helpers/pdf-link.js
+++ app/helpers/pdf-link.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import ENV from '../config/environment';
 
 export default Ember.Handlebars.makeBoundHelper(function(id){
-    var img = "<img class=\"pdf-image\" src=\"/img/pdf-file-128.png\"/>";
+    var img = "<img class=\"pdf-image\" src=\"/img/pdf-file-24.png\"/>";
     var str = "<a target=\"_blank\" href=\""+ENV.APP.fileURL+"/"+id+"\">"+img+"</a>";
     return new Ember.Handlebars.SafeString(str);
 });
