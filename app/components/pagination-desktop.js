d5349660ca2a463711ddfd8090d30de04bcfbe6b
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-11-21 19:31:14 +0100

    Reduce wide pagination by stripping out parts of it

--- app/components/pagination-desktop.js
+++ app/components/pagination-desktop.js
@@ -3,13 +3,41 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     pageArray: function() {
 	var pArray = [];
-	for(var i=0;i<this.get('pagination.pages');i++) {
-	    var p = {page: i+1};
-	    if(this.get('pagination.page') === i+1) {
-		p['active'] = true;
+	var i;
+	var p;
+	if(this.get('pagination.pages') < 10) {
+	    for(i=0;i<this.get('pagination.pages');i++) {
+		p = {page: i+1};
+		if(this.get('pagination.page') === i+1) {
+		    p['active'] = true;
+		}
+		pArray.push(p);
 	    }
-	    pArray.push(p);
+	    return Ember.ArrayProxy.create({content: Ember.A(pArray)});
+	} else {
+	    var tmpArray = [];
+	    var current_page = this.get('pagination.page') - 1;
+	    var max_page = this.get('pagination.pages') - 1;
+	    for(i=0;i<max_page+1;i++) {
+		if((i <= 1) ||
+		   ((i >= (current_page - 2)) && (i <= (current_page + 2))) ||
+		   (i >= (max_page - 1))) {
+		    p = {page: i+1};
+		    if(this.get('pagination.page') === i+1) {
+			p['active'] = true;
+		    }
+		    tmpArray.push(p);
+		} else {
+		    tmpArray.push({spacer: true});
+		}
+	    }
+	    var lastSpacer = false;
+	    tmpArray.forEach(function(item) {
+		if(lastSpacer && item.spacer) { return; }
+		pArray.push(item);
+		lastSpacer = item.spacer;
+	    });
+	    return Ember.ArrayProxy.create({content: Ember.A(pArray)});
 	}
-	return Ember.ArrayProxy.create({content: Ember.A(pArray)});
     }.property('pagination.pages', 'pagination.page'),
 });
