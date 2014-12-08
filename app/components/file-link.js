fc088c9bbfdac96e80f241741a121e29ddb70058
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-12-08 19:32:44 +0100

    PDF and image are similar. Move the logic from the template to the component

--- app/components/file-link.js
+++ app/components/file-link.js
@@ -18,18 +18,27 @@ export default Ember.Component.extend({
 	return ENV.APP.fileURL+'/'+this.get('computedFile.id');
     }.property('computedFile.id'),
     imageURL: function() {
-	return ENV.APP.fileURL+'/thumbnail/'+this.get('computedFile.id')+'?size='+this.get('size');
-    }.property('computedFile.id', 'size'),
+	if(this.get('type') === 'pdf') {
+	    return "/img/pdf-file-24.png";
+	} else {
+	    return ENV.APP.fileURL+'/thumbnail/'+this.get('computedFile.id')+'?size='+this.get('size');
+	}
+    }.property('computedFile.id', 'size', 'type'),
+    imageClass: function() {
+	if(this.get('type') === 'pdf') {
+	    return "pdf-link";
+	} else {
+	    return "image-link-thumbnail";
+	}
+    }.property('type'),
     name: function() {
 	return Ember.Handlebars.Utils.escapeExpression(this.get('computedFile.name'));
     }.property('computedFile.name'),
     typeFile: function() {
 	if(this.get('type') === 'file') { return true; }
     }.property('type'),
-    typePDF: function() {
-	if(this.get('type') === 'pdf') { return true; }
-    }.property('type'),
     typeImage: function() {
-	if(this.get('type') === 'image') { return true; }
+	var type = this.get('type');
+	if(type === 'image' || type === 'pdf') { return true; }
     }.property('type'),
 });
